3265633354705c50c5652862ae09bce5df09d088
prachi7792 <prachipednekar85@gmail.com>
2017-04-26 01:10:11 +0530

    Select expected output from the options given
    Print the Expected Output

---
+++ Java Assignment 4.3/src/com/acadgild/javaassignment4_3/core/TextDocument.java
@@ -0,0 +1,15 @@
+/**
+ * this for files
+ * @author Acer
+ * 
+ * @date Apr 26, 2017
+ */
+package com.acadgild.javaassignment4_3.core;
+
+/**
+ * this for classes
+ */
+
+ // Correct option: F
+
+//  Explanation: This is not possible because you can only refer to your immediate superclass
